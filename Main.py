0b90827bd1aabc8afee8a3e0556c3526189f4e43
YouAreMyTrap <65868083+YouAreMyTrap@users.noreply.github.com>
2021-01-18 14:03:43 +0100

    Sqlite Works
    add remove user + adduser

--- Main.py
+++ Main.py
@@ -1,4 +1,8 @@
 from SQLCall_Log import *
 
 SqlBase = SQL()
-print(SqlBase.ConnectUser("user1", "0"))
+print("Exist User: ", SqlBase.ConnectUser("user12", "0"))
+SqlBase.NewUser("user12", "0")
+print("Exist User: ", SqlBase.ConnectUser("user12", "0"))
+SqlBase.RemoveUser("user12", "0")
+print("Exist User: ", SqlBase.ConnectUser("user12", "0"))
--- SQLCall_Log.py
+++ SQLCall_Log.py
@@ -10,7 +10,7 @@ class SQL:
         """Set name and password, if this correct return True or else return False"""
         conn = sqlite3.connect(self.UserLogBD)
         c = conn.cursor()
-        c.execute('SELECT * from Login WHERE name="%s" AND passw="%s"' % (name, passwd))
+        c.execute('SELECT * from Login WHERE name="%s" AND passw="%s"' % (name, hashlib.md5(passwd.encode('utf-8')).hexdigest()))
         if c.fetchone() == None:
             return False
         else:
@@ -18,9 +18,16 @@ class SQL:
         conn.close()
 
     def NewUser(self, name , passwd):
-        print(hashlib.md5(passwd.encode('utf-8')).hexdigest())
+        """Set name and password, for create new user"""
         conn = sqlite3.connect(self.UserLogBD)
         c = conn.cursor()
-        #c.execute("INSERT INTO Users (name, pass) VALUES ('%s', %s)" %(name, hashlib.md5(passwd.encode('utf-8')).hexdigest())) 
-        #c.commit()
+        c.execute("INSERT INTO Login (name, passw) VALUES ('%s', '%s')" %(name, hashlib.md5(passwd.encode('utf-8')).hexdigest())) 
+        conn.commit()
+        conn.close()
+
+    def RemoveUser(self, name , passwd):
+        conn = sqlite3.connect(self.UserLogBD)
+        c = conn.cursor()
+        c.execute("DELETE FROM Login WHERE (name='%s' AND passw='%s')" %(name, hashlib.md5(passwd.encode('utf-8')).hexdigest())) 
+        conn.commit()
         conn.close()
