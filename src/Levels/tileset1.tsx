5f329f5d87097d94374d432243ccd42b97be5758
YouAreMyTrap <65868083+YouAreMyTrap@users.noreply.github.com>
2021-02-05 16:33:25 +0100

    TileMap Test Make :C

---
+++ src/Levels/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="tileset1" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1316" columns="28">
+ <image source="Game Boy Advance - Pokemon FireRed LeafGreen - Tileset 2.png" width="477" height="800"/>
+</tileset>
