7e7c3acdab31d379fa5261b547ec53d2cecdcc62
HenriES2022 <henriqueyeguo.yeguo@gmail.com>
2021-05-05 15:16:29 +0200

    TMX files

---
+++ src/maps/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset2" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1316" columns="28">
+ <image source="tiles/tileset2.png" width="477" height="800"/>
+</tileset>
