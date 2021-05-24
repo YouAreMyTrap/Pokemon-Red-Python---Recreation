a5b1a42416b8e27f622676c70564edc6ca26c18f
YouAreMyTrap <65868083+YouAreMyTrap@users.noreply.github.com>
2021-05-24 12:34:22 +0200

    Bug fix + Commented all text

--- src/engine/Sel_Menu.py
+++ src/engine/Sel_Menu.py
@@ -38,33 +38,23 @@ class Select_Menu:
             }
 
         print(MenuInput[self.Menu_Sel])
-        if MenuInput[self.Menu_Sel] == "EXIT": self.Menu_Loaded = False
-        if MenuInput[self.Menu_Sel] == "POKéMON": 
+        if MenuInput[self.Menu_Sel] == "EXIT": self.Menu_Loaded = False #Ecit of menu
+        if MenuInput[self.Menu_Sel] == "POKéMON":  #Go to pokeon menu
             two.Load()
             two.me = 2
-        # def KeyBind(self): #Continuo el keyimput solucionar
-            #pressed_keys = pygame.event.wait()
-            #if (pressed_keys.type == pygame.KEYDOWN) and (pressed_keys.key == pygame.K_o):
-            # self.Menu_Loaded = False if self.Menu_Loaded else True
-            
-            #if (pressed_keys.type == pygame.KEYDOWN) and (pressed_keys.key == pygame.K_UP):
-            #   self.Menu_Sel -= 1 if not self.Menu_Sel == 1 else 0
-        
 
-
-            #if self.Menu_Loaded: self.imprimir()
-            
-            #print(self.Menu_Sel)
-            #if(event.key == pygame.K_o):
-            #    print("Press o")
     def Draw(self):
-        if self.Menu_Loaded: self.print()
+        """Draw Sel ui if it's loaded"""
+        if self.Menu_Loaded: self.__print()
 
 
     def Back(self):
+        """Exit menu"""
         self.Menu_Loaded = False
 
-    def print(self):
+    def __print(self):
+        """print on screen"""
+        #Save text because I need
         Data = {
             1: [7.5, "A device that records POKéMON secrets", "upon meeting or catching them."],
             2: [22.5, "Check and organize POKéMON that are", "traveling with you in your party."],
@@ -74,24 +64,23 @@ class Select_Menu:
             6: [82.5, "Adjust various game settings such as text", "speed, game rules, etc."],
             7: [97.5, "Close this MENU window,", ""]
             }
-        #Imprime el background
-        #print(parentsource + Path + str(PLAYER_FRAME) + IMG_EXTENSION)
+        #Get image menu
         menu = pygame.image.load(parentsource + Path+ str(PLAYER_FRAME) + IMG_EXTENSION)    # 384 x 365
 
         menu_resized = pygame.transform.scale(menu, (240 * self.RESIZE, 160 * self.RESIZE))
 
 
-
+        #Get Image arrow
         arrow = pygame.image.load(parentsource +'/images/menu/Arrow.png')    # 6 x 11
         arrow_resized = pygame.transform.scale(arrow, (6 * self.RESIZE, 11 * self.RESIZE))
-
-
-        self.screen.blit(menu_resized, menu_resized.get_rect())
-        self.screen.blit(arrow_resized,(178 * self.RESIZE, Data[self.Menu_Sel][0] * self.RESIZE))
 
         #Imprime el texto
         pygame.font.init()
         
+        #Print on screen
+        self.screen.blit(menu_resized, menu_resized.get_rect())
+        self.screen.blit(arrow_resized,(178 * self.RESIZE, Data[self.Menu_Sel][0] * self.RESIZE))
+
         font = pygame.font.Font(parentsource + "/pokemon_fire_red.ttf", 15 * self.RESIZE, bold=True)
         self.screen.blit(font.render('POKéDEX', False, (96, 96, 96)) ,(185 * self.RESIZE, 5 * self.RESIZE))
         self.screen.blit(font.render('POKéMON', False, (96, 96, 96)) ,(185 * self.RESIZE, 20 * self.RESIZE))
